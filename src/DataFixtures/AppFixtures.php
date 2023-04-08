de884a9ae12f1c4fefb5d587f16445509e9ab1f6
dang <trinhdangnhat@gmail.com>
2023-04-08 12:09:17 +0200

    foundry install & set up

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
+use App\Factory\ProductFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +11,8 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
 
+        ProductFactory::createMany(20);
         $manager->flush();
     }
 }
